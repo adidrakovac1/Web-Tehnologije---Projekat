a2061704106465195635472f222bb0092a394df7
Adi Drakovac <129613464+adidrakovac1@users.noreply.github.com>
2025-01-26 17:13:04 +0100

    Metoda u modelu

--- models/nekretnine.js
+++ models/nekretnine.js
@@ -14,5 +14,13 @@ const sequelize = require("../baza.js");
     }, {
         tableName:'Nekretnine'
     });
-
+    Nekretnine.getInteresovanja = async function(nekretninaId) {
+        return await sequelize.models.Upit.findAll({
+          where: { nekretninaId },
+          include: [
+            { model: sequelize.models.Zahtjev, where: { nekretninaId }, required: false },
+            { model: sequelize.models.Ponuda, where: { nekretninaId }, required: false },
+          ]
+        });
+    };
 module.exports = Nekretnine;
